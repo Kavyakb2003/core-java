c95fb256d91c474eb8cf0feef8724b452663e9c9
Kavyakb2003 <kkb83348@gmail.com>
2026-09-08 23:37:00 +0530

    Copy Constructor

---
+++ Accessories.java
@@ -0,0 +1,22 @@
+class Accessories{
+	
+	public String name;
+	public String brand;
+	public double price;
+	public String material;
+	public boolean available;
+	
+	public Accessories()
+	{
+		
+	}
+	public Accessories(String name, String brand, double price, String material,boolean available){
+		this.name = name;
+		this.brand = brand;
+		this.price = price;
+		this.material = material;
+		this.available = available;
+	}
+	
+	
+}
